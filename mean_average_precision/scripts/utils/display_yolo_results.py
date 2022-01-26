d9874b51132b6ae9451c5060b1d36c04c761979c
Kola <office@cvml.org.ng>
2022-01-26 20:13:24 +0000

    Improve and generalise display of results

--- mean_average_precision/scripts/utils/display_yolo_results.py
+++ mean_average_precision/scripts/utils/display_yolo_results.py
@@ -41,8 +41,8 @@ else:
     bb2_list = []
 n_images = len(image_list)
 
-print(image_list)
-print(n_images)
+#print(image_list)
+#print(n_images)
 
 def onselect(eclick, erelease):
   'eclick and erelease are matplotlib events at press and release'
@@ -79,47 +79,30 @@ class Index(object):
             self.ind -= 1
         self.show_image()
 
-    def process_click(self, event, name):
-        #button = buttons[name]
-        self.ind += 1
-        ax.set_title('button clicked ' + str(self.ind))
-        print(event)
-        plt.show()
-
     def show_image(self):
-        print(self.ind)
+        #print(self.ind)
         im_path = image_list[self.ind]
         im = plt.imread(im_path)
-        (ax1, aximage1) = self._show_image(self.ax1, self.aximage1, im, 1)
-        (ax2, aximage2) = self._show_image(self.ax2, self.aximage2, im, 2)
-        #if self.ax1 is None:
-        #   self.ax1 = plt.subplot(121)
-        #if self.aximage1 is None:
-        #    self.aximage1 = self.ax1.imshow(im)
-        #else:
-        #    self.aximage1.set_data(im)
-        #    # set extents as (xmin,xmax,ymax,ymin) so y axis origin at topleft
-        #    self.aximage1.set_extent((0,im.shape[1],im.shape[0],0))
-            
-        self.set_title()
+        (self.ax1, self.aximage1) = self._show_image(self.ax1, self.aximage1, im, 1)
+        (self.ax2, self.aximage2) = self._show_image(self.ax2, self.aximage2, im, 2)
+        
+        self._set_title(self.ax1)
+        #self._set_title(self.ax2)
         self.im_width = im.shape[1]
         self.im_height = im.shape[0]
         
-        # Set the class (radio button) if it already exists in list
-        if len(image_list[self.ind]) > 1:
-            im_class = image_list[self.ind][1]
-            print( "Image is of class " + str(im_class))
-        else:
-            print("Image not yet classified")
-        #try:
+        # Plot the bounding boxes
+        self._show_bb( self.ax1, bb1_list[self.ind], "g")
+        self._show_bb( self.ax2, bb2_list[self.ind], "r")
 
         #ipdb.set_trace()
         #self.draw_rect()
         plt.draw()
     
-    def set_title(self):
-        title = "X-ray image classification - manual"
-        self.ax.set_title(title, fontsize=8)
+    def _set_title(self, ax):
+        im_name = os.path.split(image_list[self.ind])[-1]
+        title = f"Image {self.ind+1} of {n_images} - {im_name}"
+        plt.suptitle(title, fontsize=8)
 
     def _show_image(self, ax, aximage, im, ax_index):
         if ax is None:
@@ -132,15 +115,36 @@ class Index(object):
             aximage.set_extent((0,im.shape[1],im.shape[0],0))
         return (ax, aximage)
 
-def toggle_selector(event):
-    print(' Key pressed.')
-    if event.key in ['Q', 'q'] and toggle_selector.ES.active:
-        print(' EllipseSelector deactivated.')
-        toggle_selector.RS.set_active(False)
-    if event.key in ['A', 'a'] and not toggle_selector.ES.active:
-        print(' EllipseSelector activated.')
-        toggle_selector.ES.set_active(True)
+    def _show_bb(self, ax, bb_path, color='r'):
+        _ , bb1 = self._read_bounding_box(bb_path)
+        ax.lines.clear()
+        for bb in self._scale_bb(bb1, self.im_width, self.im_height):
+            ax.plot([bb[0], bb[0], bb[2], bb[2], bb[0]],
+                    [bb[1], bb[3], bb[3], bb[1], bb[1]], color)
 
+    #TODO: Move this function to a general utilities package
+    def _read_bounding_box(self, bb_path):
+        """Read bounding box for an image"""
+
+        with open(bb_path, "rt") as fid:
+            bb = [ [float(l2) for l2 in l.strip("\n").split(" ")] for l in fid.readlines()]
+    
+        bb = np.array(bb)
+    
+        # Return labels and bbs separately
+        return bb[:,0], bb[:,1:]
+    
+    #TODO: Move this function to a general utilities package
+    def _scale_bb(self, bbs, width, height):
+        scale = np.array((width, height, width, height), dtype=float)
+        bbs_scaled = []
+        for bb in bbs:
+            x_start = bb[0] - bb[2]/2
+            y_start = bb[1] - bb[3]/2
+            x_end = x_start + bb[2]
+            y_end = y_start + bb[3]
+            bbs_scaled.append(np.array((x_start, y_start, x_end, y_end) * scale, dtype=int))
+        return bbs_scaled
 
 fig = plt.figure
 global ax
@@ -167,19 +171,4 @@ for key in buttons.keys():
     button['handle'] = Button(button['axhandle'], key)
     button['handle'].on_clicked(button['callback'])
     
-# Radiobuttons
-left = 0.05
-bottom = 0.7
-width = 0.15
-height = 0.15
-offset = 0.01
-radio_buttons = {
-    'showNextImage': {'index': 3, 'callback': callback.showNextImage}, 
-    'showPrevImage': {'index': 2, 'callback': callback.showPrevImage},
-#    'saveToFile': {'index': 1, 'callback': callback.saveToFile}
-}
-
-rax = plt.axes([left,bottom,width,height])
-radio = RadioButtons(rax, ('whole mouse','skull','right paw','left paw','other'))
-
 plt.show()
